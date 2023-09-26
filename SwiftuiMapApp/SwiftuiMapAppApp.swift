c151ff96941a0b1c9d9890654753e0f8809afa1a
Han <hander.juan@gmail.com>
2023-09-26 19:32:37 +0200

    Initial Commit

---
+++ SwiftuiMapApp/SwiftuiMapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiMapAppApp.swift
+//  SwiftuiMapApp
+//
+//  Created by Juan Hander on 26/9/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiMapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
