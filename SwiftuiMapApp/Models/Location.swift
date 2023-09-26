05f120a12411183fcf57fc47ac068e1ea8aa58fa
Han <hander.juan@gmail.com>
2023-09-26 19:43:23 +0200

    Setup assets and custom data model

---
+++ SwiftuiMapApp/Models/Location.swift
@@ -0,0 +1,18 @@
+//
+//  Location.swift
+//  SwiftuiMapApp
+//
+//  Created by Juan Hander on 26/9/23.
+//
+
+import Foundation
+import MapKit
+
+struct Location {
+    let name: String
+    let cityName: String
+    let coordinates: CLLocationCoordinate2D
+    let description: String
+    let imageNames: [String]
+    let link: String
+}
